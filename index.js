8eec20160a3072c89d5a725e971cf82019cf1e5e
ashutoshongithub25 <75493680+ashutoshongithub25@users.noreply.github.com>
2020-12-05 19:47:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const http=require('http');
+const port=8000;
+const fs=require('fs');
+
+
+function requestHandler(req,res){
+    console.log(req.url);
+    res.writeHead(200,{'content-type': 'text/html'});
+    
+    let filepath;
+    switch(req.url){
+        case '/':
+            filepath='./index.html'
+            break;
+        case '/profile':
+            filepath='./profile.html'
+            break;
+          
+            default:
+                filepath='./404.html'
+
+    }
+
+    fs.readFile(filepath,function(err,data){
+        if(err){
+            console.log('err',err);
+            return res.end('<h1>error</h1>')
+        }
+        return res.end(data);
+    })
+}
+
+
+const server=http.createServer(requestHandler);
+
+server.listen(port,function(err){
+if(err){
+    console.log(err);
+    return;
+}
+else{
+    console.log("server is running on port ",port);
+}
+});
